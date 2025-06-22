12895b4eabd1c711cacfad5be000f8ed945c9d40
Ansh Sharma <anshsharma2903@gmail.com>
2025-06-22 21:14:18 +0530

    Polished app.py, more on config

--- src/app.py
+++ src/app.py
@@ -3,18 +3,25 @@ from src.interface.routes import interface_blueprint
 from src.interface.auth import auth_blueprint
 from src.config.settings import settings
 
+import sys
+from src.core.exception import CustomException
 from src.core.logger import logging
 
-logging.info("Starting the Flask application...")
-
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = settings.SECRET_KEY
-    app.register_blueprint(interface_blueprint)
-    app.register_blueprint(auth_blueprint)
-    return app
+    try:
+        logging.info("Creating Flask app")
+        app = Flask(__name__)
+        app.secret_key = settings.SECRET_KEY
+        app.register_blueprint(interface_blueprint)
+        app.register_blueprint(auth_blueprint)
+        logging.info("Flask app created successfully")
+        return app
+    except Exception as e:
+        logging.error(f"Error creating Flask app: {e}")
+        raise CustomException(sys, e)
 
 if __name__ == "__main__":
     print("Launching Flask server...")
     app = create_app()
-    app.run(debug=True) # http://127.0.0.1:5000/
+    print("server started, Host: ", settings.APP_HOST)
+    app.run(debug=settings.DEBUG) # Will run the app twice on launch
--- src/config/settings.py
+++ src/config/settings.py
@@ -6,7 +6,8 @@ load_dotenv()
 class Settings:
     SECRET_KEY = os.getenv("SECRET_KEY", "scalable_secret_key")
     DEBUG = os.getenv("DEBUG", "True") == "True"
-    OLLAMA_ENDPOINT = os.getenv("OLLAMA_ENDPOINT", "http://localhost:11434/api/generate")
-    DEFAULT_MODEL = os.getenv("OLLAMA_MODEL", "BahaSlama/llama3.1-finetuned:latest")
+    OLLAMA_ENDPOINT = os.getenv("OLLAMA_ENDPOINT")
+    DEFAULT_MODEL = os.getenv("OLLAMA_LLM")
+    APP_HOST = os.getenv("APP_HOST")
 
 settings = Settings()
